b7e6891e9385dde10649b8ef134ec169c6537559
Romain Pouclet <romain.pouclet@gmail.com>
2018-10-31 09:14:50 -0700

    Initial commit

--- SampleAppWithGPX/ViewController.swift
+++ SampleAppWithGPX/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    let location = CLLocationManager()
+
+    @IBOutlet weak var map: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        map.showsUserLocation = true
+        map.userTrackingMode = .follow
+        map.isZoomEnabled = true
+        
+        location.requestWhenInUseAuthorization()
+        location.delegate = self
+        location.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Got locations!")
+        locations.forEach { l in
+            print("- location: \(l)")
+        }
     }
 
 
